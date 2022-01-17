5b9010002da1b14cedd3d5a06e72415495afb217
Matteo Pompei <datxizzan@gmail.com>
2022-01-17 17:42:42 +0100

    Placeholder rimossi dall'array (es completato)

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 let app = new Vue ({
   el: "#app",
   data: {
-    todolist: [
-      {
-        text: "Comprare antidepressivi",
-        done: false
-      },
-      {
-        text: "Segnalare Massaro alle autorità",
-        done: true
-      },
-      {
-        text: "Portare a spasso il cane",
-        done: false
-      }
-    ],
+    todolist: [],
     inputValue: ""
   },
   methods: {
